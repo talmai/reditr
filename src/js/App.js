d426b606065def3ecf52501ab1c646e9a7688a87
Dimitry Vinogradov <dimitryvin@gmail.com>
2015-10-31 14:33:45 -0300

    Header remains!

--- src/js/App.js
+++ src/js/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="app-view">
                 <HeaderView />
                 {/*
                     next we replace `<Child>` with `this.props.children`
