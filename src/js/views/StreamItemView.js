1223cb1ffb141d9b4738a1764672b1b7132bc98e
Dimitry Vinogradov <dimitryvin@gmail.com>
2015-12-28 08:56:00 -0800

    Clickable username

--- src/js/views/StreamItemView.js
+++ src/js/views/StreamItemView.js
@@ -112,7 +112,7 @@ class StreamItemView extends React.Component {
                         <span className="stream-item-domain">({post.get("domain")})</span>
                         {postMedia}
                         <div className="mini-details">
-                            <span className="stream-item-author">{post.get("author")}</span>
+                            <Link to={"/user/" + post.get("author")} className="stream-item-author">{post.get("author")}</Link>
                             <span> posted in </span>
                             <Link to={"/r/" + post.get("subreddit")} className="stream-item-subreddit">{"/r/" + post.get('subreddit')}</Link>
                             <span> {moment.unix(post.get("created_utc")).fromNow()}</span>
